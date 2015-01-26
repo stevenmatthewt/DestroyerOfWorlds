5eb7708f28e4bc318fa762b113ee153b400313f4
Steven Thomas <stevenmatthewt@gmail.com>
2015-01-26 18:59:13 -0500

    Init

---
+++ src/main/java/Hax/Bukkit/DestroyerOfWorlds/DestroyerOfWorlds.java
@@ -0,0 +1,27 @@
+package Hax.Bukkit.DestroyerOfWorlds;
+ 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+ 
+public final class DestroyerOfWorlds extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("onEnable has been invoked!");
+    }
+ 
+    @Override
+    public void onDisable() {
+        getLogger().info("onDisable has been invoked!");
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    	if (cmd.getName().equalsIgnoreCase("basic")) { // If the player typed /basic then do the following...
+    		// doSomething
+    		return true;
+    	} //If this has happened the function will return true. 
+            // If this hasn't happened the value of false will be returned.
+    	return false; 
+    }
+}

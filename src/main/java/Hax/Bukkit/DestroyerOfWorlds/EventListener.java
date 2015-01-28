224e5f1467e6f4924885c0d69a3d5c714029f196
Steven Thomas <stevenmatthewt@gmail.com>
2015-01-27 23:15:17 -0500

    First attempt at smash style knock back

--- src/main/java/Hax/Bukkit/DestroyerOfWorlds/EventListener.java
+++ src/main/java/Hax/Bukkit/DestroyerOfWorlds/EventListener.java
@@ -4,11 +4,13 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.util.Vector;
 
 public class EventListener implements Listener {
 	DestroyerOfWorlds plugin;
@@ -20,14 +22,36 @@ public class EventListener implements Listener {
 	
     @EventHandler
     public void onEntityDamageByEntityEvent(EntityDamageByEntityEvent event) {
-    	if (!event.getEntityType().equals(EntityType.PLAYER)) {
-    		return;
+    	final Player hurt;
+    	Player damager;
+    	if (event.getEntityType().equals(EntityType.PLAYER)) {
+    		hurt = ((Player) event.getEntity());
+    		
+    		final double knockbackFactor = hurt.getMaxHealth()/hurt.getHealth();
+        	plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+                @Override
+                public void run() {
+                    Vector knockback = hurt.getVelocity().multiply(knockbackFactor);
+                    hurt.setVelocity(knockback);
+                    System.out.println(hurt.getVelocity());
+                    System.out.println(knockbackFactor);
+                }
+            }, 1L);
+    		
+    		if (event.getDamager().getType().equals(EntityType.PLAYER)) {
+        		damager = (Player) event.getDamager();
+        	} else if (event.getDamager() instanceof Projectile) {
+        		if (((Projectile) event.getDamager()).getShooter() instanceof Player) {
+        			damager = (Player) ((Projectile) event.getDamager()).getShooter();
+        		} else {
+        			return;
+        		}
+        	} else {
+        		return;
+        	}
+    		
+    		plugin.setLastDamagedBy(hurt.getName(), damager.getName());
     	}
-    	
-    	if (!(((Projectile) event.getDamager()).getShooter() instanceof Player)) {
-    		return;
-    	}
-    	plugin.setLastDamagedBy(event.getEntity().getName(), ((Player)((Projectile) event.getDamager()).getShooter()).getName());
     }
     
     @EventHandler
